2d159f93cd0c1e934e208e566ba3466e64be4536
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-03-27 14:38:03 +0000

    exercicio resolvido

---
+++ main.py
@@ -0,0 +1,7 @@
+yearNow = int(input("Digite o ano atual: "))
+ageNow = int(input("Digite sua idade atual: "))
+yearFuture = int(input("Digite um ano no futuro: "))
+name = input("Digite seu nome: ")
+ageFuture = (yearFuture - ageNow ) + ageNow
+
+print(f"{name}, no ano de {yearFuture} você terá {ageFuture} anos ")
